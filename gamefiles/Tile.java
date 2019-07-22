a736a2e737ea7d6885688b3fa1c757591fdb9291
reen145 <36958048+reen145@users.noreply.github.com>
2019-07-22 15:21:42 -0400

    Icon updates

--- gamefiles/Tile.java
+++ gamefiles/Tile.java
@@ -67,7 +67,7 @@ public class Tile extends JButton {
         iconPath = new ImageIcon("Icons/pathCobble.png");
         iconChest = new ImageIcon("Icons/chest.png");
         iconKey = new ImageIcon("Icons/key.png");
-        iconPlayerKey = new ImageIcon("Icons/player_key.png");
+        iconPlayerKey = new ImageIcon("Icons/knight_key.png");
         iconChestOpen = new ImageIcon("Icons/chest_open.png");
     	type = ti;
     	updateImage();
