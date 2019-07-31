74e383766bb0b6cf3ebf4a7e369cc8396822c153
reen145 <36958048+reen145@users.noreply.github.com>
2019-07-31 15:03:57 -0400

    Updated tests

--- tests/MyTests.java
+++ tests/MyTests.java
@@ -18,26 +18,45 @@ import gamefiles.GamePanel;
 class MyTests {
 	/**
 	 * Class for testing that the Maze Game GUI can properly initialize
+	 * Will fail if the game GUI constructor fails at any point
 	 **/
 	@Test
 	public void testGameGUI() {
+		//tests initialization of a new GameGUI
 		GameGUI tester = new GameGUI(10, 64);
+		assertNotNull(tester);
 	}
 	
 	/**
 	 * Class for testing that the Maze Game swing panel works correctly
+	 * Will fail if any of the functionality for the GamePanel class fails
 	 **/
 	@Test
 	public void testGamePanel() {
+		//tests initialization of a new gamepanel
 		GamePanel panel = new GamePanel(10);
+		//gets the action and input maps from the panel
 		ActionMap amap = panel.getActionMap();
 		InputMap imap = panel.getInputMap();
+		//ensures that the action and input maps are not null
 		assertNotNull(amap);
 		assertNotNull(imap);
+		//attempts to execute the populate board method
 		panel.populate();
+		//attempts to play each of the four sound effects
 		panel.playChest();
 		panel.playError();
 		panel.playKey();
 		panel.playStep();
+	}
+	
+	/**
+	 * Class for testing that the Maze Game model works correctly
+	 * Will fail if any of the functionality for the GameModel fails
+	 **/
+	@Test
+	public void testGameModel() {
+		GameModel model = new GameModel();
+		
 	}
 }
